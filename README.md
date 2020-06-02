59122bf3ee318b6fb07da2bcd816e4827b8246b0
Matthijs van der Veer <m.van.der.veer@avanade.com>
2020-06-02 16:25:14 +0200

    Initial commit based on starter blog.

---
+++ README.md
@@ -0,0 +1,2 @@
+## vanderveer.io
+My simple blog built on Gatsby. Started from the [Gatsby Starter Blog](https://github.com/gatsbyjs/gatsby-starter-blog)
---
+++ gatsby-config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  siteMetadata: {
+    title: `Matthijs van der Veer's Blog`,
+    author: {
+      name: `Matthijs van der Veer`,
+      summary: `who works as an IoT Specialist in Avanade's Digital Innovation Studio.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://www.vanderveer.io/`,
+    social: {
+      twitter: `MatthijsvdVeer`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Matthijs van der Veer's Blog`,
+        short_name: `Matthijs van der Veer's Blog`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
